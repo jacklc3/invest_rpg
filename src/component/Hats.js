697dee7b60a13bfa39fdfb65e6f281576397a720
xsamhallx <54696339+xsamhallx@users.noreply.github.com>
2021-04-03 17:36:28 +1100

    switch hat working

--- src/component/Hats.js
+++ src/component/Hats.js
@@ -1,5 +1,5 @@
 import "./Hats.css";
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function importAll(r) {
     let images = {};
@@ -7,46 +7,26 @@ function importAll(r) {
     console.log(images);
     return images;
   }
-
-//   ---need to do this but using the effect hook----
-//   function countReset(count,arr, update) {
-//       if (count === arr.length) {
-            // initialCount = 0;
-//           console.log('count is reset')
-//       } 
-//   }
   
-  const images = importAll(require.context('./img/hats', false, /\.(png|jpe?g|svg)$/));
-  
-//   <img src={images['doggy.png']}
+const images = importAll(require.context('./img/hats', false, /\.(png|jpe?g|svg)$/));
 
 const Hats = () => {
     const initialCount = 0;
     const [count, setCount] = useState(initialCount);
     const hatsArr = ["archerHat", "bowlerHat", "catEars", "crown", "headphones", "jesterHat", "magicianHat", "purplePartyHat", "fedora", "halo"];
-   
-    //something like this. only problem now is it's not picking up the function change in render...
-    const nextHat = () => {
-      setCount(prevCount => prevCount + 1);
-     }
-  
-     const initialHat = () => {
-      setCount(initialCount);
-     }
-  
-     let hatButton = nextHat;
-    
-    useEffect(() => {
+      
+    const switchItem = (arr) => e => {
       switch (count) {
-        default: hatButton = initialHat;
-        break;
+      case arr.length - 1: setCount(initialCount);
+      break;
+      default: setCount(prevCount => prevCount + 1);
+      break;
       }
-      console.log(hatButton);
-    });
+     }
     
     return ( 
         <div className="hats">
-        <button onClick={ hatButton }>
+        <button onClick={ switchItem(hatsArr) }>
           <img src={ images[hatsArr[count]+'.png'].default } alt="an adventurers hat"/>
         </button>
         </div>
